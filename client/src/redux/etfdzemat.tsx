3e91a603fddb18b2e308986847acd62a4c6e41e9
Anand Pap <anandpap@live.com>
2025-01-20 18:18:02 +0100

    Dodana upotreba redux-a za global state management.

---
+++ client/src/redux/etfdzemat.tsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import type { PayloadAction } from "@reduxjs/toolkit";
+
+// export interface InitialState {}
+
+const initialState = {};
+
+export const etfdzematSlice = createSlice({
+  name: "etfdzemat",
+  initialState,
+  reducers: {},
+});
+
+// export const {} = etfdzematSlice.actions;
+
+export default etfdzematSlice.reducer;
