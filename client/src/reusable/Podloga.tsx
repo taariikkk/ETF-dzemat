e5c1501b2488a1b3b807f793f063b57c78d98931
Anand Pap <anandpap@live.com>
2025-01-21 08:11:32 +0100

    Napravljena podloga.

--- client/src/reusable/Podloga.tsx
+++ client/src/reusable/Podloga.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Podloga: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="">{children}</div>;
+  return <div className="max-w-96 px-6 py-8 bg-gray-100 w-5/6 mx-auto rounded-lg">{children}</div>;
 };
 
 export default Podloga;
