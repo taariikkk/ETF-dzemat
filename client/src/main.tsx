fe2c1cd15fd03a300d40c9b7976352f2a04a5653
Anand Pap <anandpap@live.com>
2025-02-22 06:15:08 +0100

    Dodan vercel paket za pracenje analize prometa stranice.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,11 @@ import "./styles/index.css";
 import Router from "./Router.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <Provider store={store}>
       <Router />
     </Provider>
