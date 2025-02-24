069f28da116cd22d3b844ff019054200eb806e8c
Anand Pap <anandpap@live.com>
2025-02-24 23:34:13 +0100

    Dgume prima tip.

--- client/src/reusable/Dugme.tsx
+++ client/src/reusable/Dugme.tsx
@@ -2,18 +2,19 @@ import { twMerge } from "tailwind-merge";
 
 interface DugmeProps {
   text: string;
+  type?: "submit" | "reset" | "button" | undefined;
   className?: string;
   onClick?: () => void;
 }
 
-const Dugme: React.FC<DugmeProps> = ({ text, className, onClick }) => {
+const Dugme: React.FC<DugmeProps> = ({ text, className, ...props }) => {
   return (
     <button
-      onClick={onClick}
       className={twMerge(
         "px-8 border-2 border-black duration-200 bg-gray-100 hover:bg-stone-200 active:bg-gray-100 rounded-lg shadow-md transition-all",
         className
       )}
+      {...props}
     >
       {text}
     </button>
