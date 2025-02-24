d548eb17326fd3851c2929c0784517d3da77f971
Anand Pap <anandpap@live.com>
2025-02-25 00:01:02 +0100

    Povezana registracija sa serverom.

--- client/src/main-components/Registracija.tsx
+++ client/src/main-components/Registracija.tsx
@@ -5,16 +5,26 @@ import NaslovStranice from "../reusable/NaslovStranice";
 import Dugme from "../reusable/Dugme";
 import Input from "../reusable/Input";
 import Podloga from "../reusable/Podloga";
+import { signup } from "../helper-functions/fetch-functions";
+import { getErrorMessage } from "../helper-functions/error-functions";
 
 const inputPolja = [
-  { type: "text", placeholder: "Korisničko ime" },
-  { type: "text", placeholder: "Email" },
-  { type: "password", placeholder: "Šifra" },
-  { type: "password", placeholder: "Potvrdite šifru" },
+  { type: "text", placeholder: "Korisničko ime", valueName: "username" },
+  { type: "text", placeholder: "Email", valueName: "email" },
+  { type: "password", placeholder: "Šifra", valueName: "password" },
+  { type: "password", placeholder: "Potvrdite šifru", valueName: "confirmPassword" },
 ];
 
+export interface Registracija {
+  [key: string]: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const Registracija = () => {
-  const [registracija, setRegistracija] = useState({
+  const [registracija, setRegistracija] = useState<Registracija>({
     username: "",
     email: "",
     password: "",
@@ -29,17 +39,38 @@ const Registracija = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleRegister = () => {
+  function validateEmailFormat(email: string) {
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regEx.test(email);
+  }
+
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { username, email, password, confirmPassword } = registracija;
+    console.log(username);
+
     if (Object.values(registracija).some((value) => value === "")) setError("Sva polja moraju biti popunjena");
-    else if (registracija.password !== registracija.confirmPassword) setError("Šifre se ne podudaraju");
-    else navigate("/prijava");
+    if (username.length < 2) setError("Korisniško ime mora imati više od dva znaka");
+    else if (!validateEmailFormat(email)) setError("Neispravan email");
+    else if (password.length < 6) setError("Šifra mora biti 6 znakova duga");
+    else if (/\s/.test(password)) setError("Šifra ne smije sadržavati praznine");
+    else if (!confirmPassword || password !== confirmPassword) setError("Šifre se ne podudaraju");
+    else
+      signup(registracija)
+        .then((res) => {
+          if (res && "message" in res) {
+            navigate("/prijava");
+          } else setError(getErrorMessage(res));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, valueName: string) => {
     setRegistracija((s) => ({
       ...s,
-      [name]: value,
+      [valueName]: e.target.value,
     }));
   };
 
@@ -48,15 +79,22 @@ const Registracija = () => {
       <>
         <NaslovStranice naslovStranice="Registracija" />
         <Podloga classname="bg-transparent px-0 py-0 max-w-72">
-          <form>
+          <form onSubmit={(e) => handleRegister(e)}>
             {inputPolja.map((props, i) => {
-              return <Input key={i} onChange={handleChange} {...props} />;
+              return (
+                <Input
+                  key={i}
+                  {...props}
+                  value={registracija[props.valueName]}
+                  onChange={(e) => handleChange(e, props.valueName)}
+                />
+              );
             })}
+            {error && <p className="text-red-500 leading-normal">{error}</p>}
+            <div className="mt-4">
+              <Dugme text="Registruj se" type="submit" />
+            </div>
           </form>
-          {error && <p className="text-red-500">{error}</p>}
-          <div className="mt-4">
-            <Dugme text="Registruj se" onClick={handleRegister} />
-          </div>
           <p className="mt-4">
             Već ste registrovani?{" "}
             <a onClick={() => navigate("/prijava")} className="underline cursor-pointer">
