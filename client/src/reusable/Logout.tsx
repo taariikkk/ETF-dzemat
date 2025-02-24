0eeb83090905a735ad807c1f2fe671db903e8878
Anand Pap <anandpap@live.com>
2025-02-25 00:35:01 +0100

    Dodan logout fetch.

--- client/src/reusable/Logout.tsx
+++ client/src/reusable/Logout.tsx
@@ -2,20 +2,21 @@ import { MdLogout } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { setPrijavljen } from "../redux/etfdzemat";
+import { signout } from "../helper-functions/fetch-functions";
 
 const Logout = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const handleLogout = async () => {
+    await signout();
+    dispatch(setPrijavljen(false));
+    navigate("/prijava");
+  };
+
   return (
     <div className="absolute top-2 left-6 cursor-pointer">
-      <MdLogout
-        onClick={() => {
-          dispatch(setPrijavljen(false));
-          navigate("/prijava");
-        }}
-        size={30}
-      />
+      <MdLogout onClick={handleLogout} size={30} />
     </div>
   );
 };
