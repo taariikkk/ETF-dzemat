a80662fb73508710eafc0561a05fc04ade1796aa
Anand Pap <anandpap@live.com>
2025-02-24 13:37:36 +0100

    Dugme komponenti moguce proslijediti className prop.

--- client/src/reusable/Dugme.tsx
+++ client/src/reusable/Dugme.tsx
@@ -1,13 +1,19 @@
+import { twMerge } from "tailwind-merge";
+
 interface DugmeProps {
   text: string;
+  className?: string;
   onClick?: () => void;
 }
 
-const Dugme: React.FC<DugmeProps> = ({ text, onClick }) => {
+const Dugme: React.FC<DugmeProps> = ({ text, className, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-8 border-2 border-black duration-200 bg-gray-100 hover:bg-stone-200 active:bg-gray-100 rounded-lg shadow-md transition-all"
+      className={twMerge(
+        "px-8 border-2 border-black duration-200 bg-gray-100 hover:bg-stone-200 active:bg-gray-100 rounded-lg shadow-md transition-all",
+        className
+      )}
     >
       {text}
     </button>
