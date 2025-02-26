7e071f56f48ce8ae4898d57c4a2d6da0e99b4078
Anand Pap <anandpap@live.com>
2025-02-26 02:15:00 +0100

    Prepravljena stranica ShoppingLista.

--- client/src/main-components/ShoppingLista.tsx
+++ client/src/main-components/ShoppingLista.tsx
@@ -3,75 +3,97 @@ import { useState } from "react";
 import { FaShoppingCart, FaPlus, FaTrashAlt } from "react-icons/fa";
 import Podloga from "../reusable/Podloga";
 import Input from "../reusable/Input";
+import { useAppSelector } from "../redux/hooks";
+
+const shoppingListItems = [
+  { naziv: "Trulex", checked: false },
+  { naziv: "Otirač", checked: false },
+  { naziv: "Ubrusi", checked: false },
+  { naziv: "Četka za pod", checked: true },
+  { naziv: "Konvektor", checked: true },
+];
 
 const ShoppingLista = () => {
-  const [items, setItems] = useState<string[]>([]);
-  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+  const [items, setItems] = useState(shoppingListItems);
   const [newItem, setNewItem] = useState("");
+  const { userInfo } = useAppSelector((s) => s.etfszm);
 
   const handleAddItem = () => {
     if (newItem.trim() !== "") {
-      setItems((s) => [...s, newItem]);
+      setItems((s) => [...s, { naziv: newItem.trim(), checked: false }]);
       setNewItem("");
     }
   };
 
-  const handleCheck = (item: string) => {
-    if (checkedItems.includes(item)) {
-      setCheckedItems((s) => s.filter((it) => it !== item));
-      setItems((s) => [...s, item]);
-    } else {
-      setCheckedItems((s) => [...s, item]);
-      setItems((s) => s.filter((it) => it !== item));
-    }
+  const handleCheck = (i: number) => {
+    setItems((s) => s.map((item, index) => (i === index ? { ...item, checked: !item.checked } : item)));
   };
 
   const handleDeleteItem = (itemID: number) => {
-    setCheckedItems((s) => s.filter((_, i) => i !== itemID));
+    setItems((s) => s.filter((_, i) => i !== itemID));
   };
 
   return (
     <>
       <NaslovStranice naslovStranice={"Shopping lista"} />
-      <Podloga classname="text-4xl">
+      <Podloga>
         <div className="bg-white p-4 rounded shadow-md min-h-48 mx-auto">
           <h2 className="flex font-semibold items-center text-xl mb-3">
             <FaShoppingCart className="mr-2" size={22} /> Korpa
           </h2>
-          <ul>
-            {items.map((item, i) => (
-              <li key={i} className="flex items-center space-x-2">
-                <input checked={false} type="checkbox" onChange={() => handleCheck(item)} className="accent-blue-500" />
-                <span className="text-black">{item}</span>
-              </li>
-            ))}
+          <ul className="mt-4">
+            {items.map(
+              (item, i) =>
+                !item.checked && (
+                  <li key={i} className="flex items-center space-x-2 mt-1">
+                    <input
+                      checked={item.checked}
+                      type="checkbox"
+                      onChange={() => handleCheck(i)}
+                      className="accent-blue-500"
+                    />
+                    <span className="text-black">{item.naziv}</span>
+                  </li>
+                )
+            )}
           </ul>
           <div className="flex items-center mt-2">
-            <button onClick={handleAddItem} className="text-blue-500 mr-2">
-              <FaPlus />
-            </button>
             <Input
               type="text"
               value={newItem}
               onChange={(e) => setNewItem(e.target.value)}
-              className="border px-1 py-0 my-1"
+              className="border py-2"
               placeholder="Dodaj stavku"
             />
+            <button onClick={handleAddItem} className="text-blue-500 ml-2">
+              <FaPlus />
+            </button>
           </div>
         </div>
-        {checkedItems.length > 0 && (
+        {items.some((item) => item.checked) && (
           <div className="bg-white p-4 rounded shadow-md mt-4 min-h-48">
-            <h2 className="font-semibold text-left text-xl mb-2">Kupljene stavke ({checkedItems.length}):</h2>
-            <ul>
-              {checkedItems.map((item, i) => (
-                <li key={i} className="flex items-center text-gray-500">
-                  <input type="checkbox" checked onChange={() => handleCheck(item)} className="accent-blue-500" />
-                  <span className="ml-2 line-through">{item}</span>
-                  <div className="ml-auto">
-                    <FaTrashAlt className="cursor-pointer ml-auto" size={17} onClick={() => handleDeleteItem(i)} />
-                  </div>
-                </li>
-              ))}
+            <h2 className="font-semibold text-left text-xl mb-2">
+              Kupljene stavke ({items.filter((item) => item.checked).length}):
+            </h2>
+            <ul className="mt-4">
+              {items.map(
+                (item, i) =>
+                  item.checked && (
+                    <li key={i} className="flex items-center text-gray-500 mt-1">
+                      <input type="checkbox" checked onChange={() => handleCheck(i)} className="accent-blue-500" />
+                      <span className="ml-2 line-through">{item.naziv}</span>
+                      {userInfo.role === "admin" && (
+                        <div className="ml-auto">
+                          <FaTrashAlt
+                            className="cursor-pointer ml-auto"
+                            size={17}
+                            onClick={() => handleDeleteItem(i)}
+                          />
+                        </div>
+                      )}
+                    </li>
+                  )
+              )}
             </ul>
           </div>
         )}
